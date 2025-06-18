f975f6520a0e329767099a27834f200790db8ccb
Philip Tricca <flihp@twobit.org>
2025-06-18 11:33:54 -0700

    Replace artifacts from `test-keys/*` w/ generated test PKI
    This commit removes all keys, certs and cert chains that were checked
    into the repo for use in unit tests & example programs. In their place
    we use the artifacts generated by the `pki-playground` tool using the
    test pki from `tls/test-keys/config.kdl`. `build.rs` has been updated to
    generate these for us. We can't use `#[cfg(test)]` in `build.rs` so we
    implement a bit of a hack using a feature `unittest` that we require
    through the `dev-dependencies` block.
    
    Github CI workflows have been updated to install `pki-playground` as
    it's now required to build our test data. The `macos-latest` builder
    can't build this tool (linking fails) so we've removed it from the test
    matrix.

--- tls/build.rs
+++ tls/build.rs
@@ -2,16 +2,53 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use anyhow::Result;
+#[cfg(feature = "unittest")]
+use anyhow::{Context, anyhow};
+
 /// This path is where Oxide specific libraries live on helios systems.
 /// This is needed for linking with libipcc
 #[cfg(target_os = "illumos")]
 static OXIDE_PLATFORM: &str = "/usr/platform/oxide/lib/amd64/";
 
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
+#[cfg(feature = "unittest")]
+fn pki_gen_cmd(cmd: &str) -> Result<()> {
+    let output = std::process::Command::new("pki-playground")
+        .arg(cmd)
+        .output()?;
+
+    if !output.status.success() {
+        let stdout = String::from_utf8(output.stdout).unwrap();
+        println!("stdout: {stdout}");
+        let stderr = String::from_utf8(output.stderr).unwrap();
+        println!("stderr: {stderr}");
+
+        return Err(anyhow!("pki-playground failed"));
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
     #[cfg(target_os = "illumos")]
     {
         println!("cargo:rustc-link-arg=-Wl,-R{}", OXIDE_PLATFORM);
         println!("cargo:rustc-link-search={}", OXIDE_PLATFORM);
     }
+
+    #[cfg(feature = "unittest")]
+    {
+        let start_dir = std::env::current_dir().context("get current dir")?;
+        std::env::set_current_dir("test-keys/")
+            .context("chdir to test keys")?;
+
+        pki_gen_cmd("generate-key-pairs")?;
+        pki_gen_cmd("generate-certificates")?;
+        pki_gen_cmd("generate-certificate-lists")?;
+
+        std::env::set_current_dir(start_dir)
+            .context("restore current dir to original")?;
+    }
+
+    Ok(())
 }
